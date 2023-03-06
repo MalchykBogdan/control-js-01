69f04a634b2bd18aca40ce73ab10b3f2127c265b
MalchykBogdan <124812110+MalchykBogdan@users.noreply.github.com>
2023-03-06 17:26:07 +0200

    Add files via upload

---
+++ task_7.js
@@ -0,0 +1,31 @@
+const allLogins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function (login) {
+    return login.length >= 4 && login.length <= 16;
+};
+
+const addLogin = function (login) {
+    if (!isLoginValid(login)) {
+      return 'Помилка! Логін повинен бути від 4 до 16 символів';
+    }
+  
+    if (!isLoginUnique(login)) {
+      return 'Такий логін уже використовується!';
+    }
+  
+    allLogins.push(login);
+    return 'Логін успішно доданий!';
+};
+
+
+const isLoginUnique = function (login) {
+    if (allLogins.includes(login)) {
+        return false;
+    }
+    return true;
+};
+
+const userLogin = prompt('Ведіть свій логін:');
+const result = addLogin(userLogin);
+alert(result);
+console.log(allLogins);
